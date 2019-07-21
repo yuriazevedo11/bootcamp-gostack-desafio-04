b71a4ed33b4d052b71c4dbcce47a8ee420bdcfd6
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-07-21 10:19:53 -0300

    NEW - Cabeçalho da página

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Header from './components/Header'
+
 import './App.css'
 
 const App = () => (
   <>
-    <h1>React Facebook</h1>
+    <Header />
   </>
 );
 
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <nav>
+      <img src="https://i.imgur.com/KDIDiSE.png" />
+      <section>
+        <span>Meu perfil</span>
+        <i class="fas fa-user-circle"></i>
+      </section>
+    </nav>
+  </header>
+);
+
+export default Header;
